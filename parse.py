1db807ab2b130b53d583018a596a12b6113d2f8a
knod <knodding@gmail.com>
2019-08-19 20:55:16 -0400

    Inits parsing

---
+++ parse.py
@@ -0,0 +1,3 @@
+# parse.py
+
+# Name, DOB, Aliases, Location, Docket #, Status (Open or Closed), Disp Event Date, Last Action Date, Statute, Grade, Description, Disposition, OTN (offense tracking #), Last Action, Processing status
